9f4981fc36b03b76ac09a909289c50323c8bc011
EmmaPotthastBJSS <emma.potthast@bjss.com>
2025-05-06 13:25:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ pd_df=my_dataframe.to_pandas()
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients: ",
     my_dataframe
-    ,max_selections=6
+    ,max_selections=5
 )
 
 if ingredients_list:
@@ -49,8 +49,8 @@ if ingredients_list:
 
     st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-            values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
+            values ('""" + ingredients_string + """')"""
 
     #st.write(my_insert_stmt)
     time_to_insert = st.button("Submit Order")
